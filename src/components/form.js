ee6693bd6e37ca2a7c8a27eaeae477554cbafc09
Mortron <cmsl4y3r@gmail.com>
2021-11-12 08:28:28 -0700

    form partially working

--- src/components/form.js
+++ src/components/form.js
@@ -2,16 +2,25 @@ import React, { useState } from 'react';
 
 const Form = (props) => {
     const {box, setBox} = props;
+    const [color, setColor] = useState("");
 
 
+    const submiHandler= (e) => {
+        e.preventDefault(e);
+
+        setBox([...box, {
+            color,
+        }])
+        setColor("");
+    };
 
     return(
         <div>
             <h1>Add A Box!</h1>
-            <form>
+            <form onSubmit={submiHandler} className="boxForm">
                 <label htmlFor="color">Color : </label>
                 {/* Need to see how to do the color here */}
-                <input name="color" type="text" ></input>
+                <input name="color" type="text" value={color} onChange={(e) => setColor(e.target.value)} />
                 <button type="submit">Add</button>
             </form>
         </div>
